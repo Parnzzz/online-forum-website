9ab6106d6d5c200cdf86ac89e7cfd025b6234c57
Parnzzz <114709549+Parnzzz@users.noreply.github.com>
2025-04-24 20:31:17 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   images:{
     domains:["plus.unsplash.com","images.unsplash.com","picsum.photos", "fastly.picsum.photos", "media.istockphoto.com" ]
   },
- 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
