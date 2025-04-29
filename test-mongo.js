3f9d8878d5a79d776c76752eaf131293d0930770
Parnzzz <114709549+Parnzzz@users.noreply.github.com>
2025-04-29 14:57:04 +0700

    Update test-mongo.js

--- test-mongo.js
+++ test-mongo.js
@@ -1,12 +1,1 @@
-import mongoose from 'mongoose';
-import User from './models/user.js'; // ปรับ path ตามจริง
 
-await mongoose.connect("mongodb+srv://..."); // ใช้ MONGODB_URI ของคุณ
-
-const newUser = await User.create({
-  name: 'test',
-  email: 'test@example.com',
-  password: '123456',
-});
-
-console.log("User saved:", newUser);
